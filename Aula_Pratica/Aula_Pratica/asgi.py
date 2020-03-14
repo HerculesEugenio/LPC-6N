72af7ed208731de834da38dbdc355026590781ef
Hercules Eugênio <61092668+HerculesEugenio@users.noreply.github.com>
2020-03-13 21:39:39 -0300

    Add files via upload

---
+++ Aula_Pratica/Aula_Pratica/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Aula_Pratica project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Aula_Pratica.settings')
+
+application = get_asgi_application()
